b1ceff36c329e52229fb89d66f2dff0950ffc3e9
Jayk <jay.khavadia@rlogical.com>
2024-11-08 11:42:36 +0530

    exported user model

--- models/User.js
+++ models/User.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  // Add additional fields as needed
+  // Additional fields as needed
 });
 
-module.exports = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
